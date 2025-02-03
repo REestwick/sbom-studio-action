7ca013198a9e000fe04516dbc5cae37d7bdccb97
Richard Estwick <restwick@cybeats.com>
2025-02-03 17:15:15 -0500

    alter webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 import path from "path";
+import { fileURLToPath } from "url";
 
-module.exports = {
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
   entry: './index.js', // Entry point for the action code
   output: {
     path: path.resolve(__dirname, 'dist'), // Output directory
     filename: 'index.js', // Bundled output file
-    libraryTarget: 'umd', // Universal Module Definition (works with Node, AMD, and CommonJS)
+    // libraryTarget: 'umd', // Universal Module Definition (works with Node, AMD, and CommonJS)
+    library: {
+        type: "module",
+      }
   },
   mode: 'production', // Bundle for production (minification, etc.)
-  target: 'node', // GitHub Actions runs in a Node.js environment
+//   target: 'node', // GitHub Actions runs in a Node.js environment
+experiments: {
+    outputModule: true,
+  }
 };
